3b726be766bbeb2ed89111d8912d0079fdf1be82
karthikeyangvk <karthikeyangskumar@gmail.com>
2025-11-14 03:22:48 +0530

    Update route.js

--- GeminiChat-Bot/route.js
+++ GeminiChat-Bot/route.js
@@ -1,17 +1,15 @@
 import {GoogleGenerativeAI} from "@google/generative-ai";
 import {NextResponse} from "next/server";
-
 export async function POST(request){
+    
     try {
         const {prompt} = await request.json();
 
         if(!prompt){
             return NextResponse.json({ message: 'Prompt is required'}, {status: 400})
         }
-
         const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
-
         const result = await model.generateContent(prompt);
         const response = await result.response;
         const text = response.text();
@@ -21,4 +19,5 @@ export async function POST(request){
         console.error("Error generating content:", error);
         return NextResponse.json({message: "Internal server error"}, {status: 500});
     }
+
 }
